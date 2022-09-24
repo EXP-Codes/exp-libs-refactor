533c15a86901d7cbad223148a307407cda27edf6
EXP <289065406@qq.com>
2022-09-24 23:59:23 +0800

    新增字符串匹配算法： 暴力（ java 最快）

--- exp-libs-algorithm/src/main/java/exp/libs/algorithm/substr/BruteForce.java
+++ exp-libs-algorithm/src/main/java/exp/libs/algorithm/substr/BruteForce.java
@@ -2,7 +2,13 @@ package exp.libs.algorithm.substr;
 
 /**
  * <PRE>
- * 字符串模式匹配：暴力匹配 （java 默认，但是被优化得超高效 ？）
+ * 字符串模式匹配：暴力匹配 （java 默认）
+ * 时间复杂度： O(m*n)
+ * ---------------------------------------------
+ * 但是实际上在 java 暴力是最快的，
+ * 因为其他算法都需要 memory copy 过程去空间换时间，但是 java 的空间是比较昂贵的，
+ * 导致某些场景下耗时远超算法本身，
+ * 暴力因为不需要开辟新空间，反而是最快的。
  * </PRE>
  * <br/><B>PROJECT : </B> exp-libs
  * <br/><B>SUPPORT : </B> <a href="https://exp-blog.com" target="_blank">https://exp-blog.com</a>
