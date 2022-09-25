97f687b425576b3ca0a0d5015e7f8d5f64b6d05f
exp.liao <289065406@qq.com>
2022-09-25 13:06:56 +0800

    Update gen_javadoc.sh

--- bin/gen_javadoc.sh
+++ bin/gen_javadoc.sh
@@ -7,7 +7,7 @@
 # ------------------------------------------------
 
 echo "Generate javadoc-jar for all modules ..."
-#mvn clean package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true
 
 items=`ls | grep 'exp-libs-'`
 for item in $items
