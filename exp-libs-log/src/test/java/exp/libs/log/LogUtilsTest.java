2bd48262b9076db6d4e6dec0be797e11d0e147cf
exp.liao <289065406@qq.com>
2022-10-03 01:09:30 +0800

    支持 Log4j2

--- exp-libs-log/src/test/java/exp/libs/log/LogUtilsTest.java
+++ exp-libs-log/src/test/java/exp/libs/log/LogUtilsTest.java
@@ -18,11 +18,11 @@ class LogUtilsTest {
 
     private static Logger log = LoggerFactory.getLogger(LogUtilsTest.class);
 
-    /** 此工程为子模块，所以工程的根目录是父工程的目录，故需在路径前添加 exp-libs-log */
+    /** 此工程为子模块，logback 的工程的根目录是父工程的目录，故需在路径前添加 exp-libs-log */
     private final static String LOGBACK_PATH = "./exp-libs-log/conf/logback.xml";
 
-    /** 此工程为子模块，所以工程的根目录是父工程的目录，故需在路径前添加 exp-libs-log */
-    private final static String LOG4J2_PATH = "./exp-libs-log/conf/log4j2.xml";
+    /** 此工程为子模块，log4j2 的工程的根目录是子模块的目录，故需路径前无需添加 exp-libs-log */
+    private final static String LOG4J2_PATH = "./conf/log4j2.xml";
 
 
     @Test
@@ -41,6 +41,15 @@ class LogUtilsTest {
 
     @Test
     void loadLog4J2Config() {
+        log.debug("[DEBUG] 尚未初始化 log4j2 日志");
+        log.info("[INFO] 尚未初始化 log4j2 日志");
+        log.warn("[WARN] 尚未初始化 log4j2 日志");
+        log.error("[ERROR] 尚未初始化 log4j2 日志");
 
+        LogUtils.loadLog4JConfig(LOG4J2_PATH);
+        log.debug("[DEBUG] 已初始化 log4j2 日志");
+        log.info("[INFO] 已初始化 log4j2 日志");
+        log.warn("[WARN] 已初始化 log4j2 日志");
+        log.error("[ERROR] 已初始化 log4j2 日志");
     }
 }
